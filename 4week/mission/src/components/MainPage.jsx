c10743a076ee53d3491b4350d4475ca1ed4dc637
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-04-27 01:52:01 +0900

    4week commit

---
+++ 4week/mission/src/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import './MainPage.css'
+function MainPage() {
+  
+  return (
+    <div className="main">
+      <h1>🎬환영합니다🎬</h1>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ 4week/mission/src/components/NowPlayingPage.jsx
@@ -0,0 +1,51 @@
+import  { useState, useEffect } from 'react';
+import './PopularPage.css';
+import { Link } from 'react-router-dom';
+
+function NowPlayingPage() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=6c60e7f9faa167c5a152da49115e39ee')
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results);
+        setLoading(false);
+      })
+      .catch(error => console.error('Error fetching movies:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? ( // 로딩 중인 경우
+        <div className="loading-spinner-container">
+          <div className="loading-spinner"></div> {/* Spinner 표시 */}
+          <p>Loading...</p> {/* 로딩 중이라는 메시지 */}
+        </div>
+      ) : (
+        <div className="movie-list">
+          {movies.map(movie => (
+            <div key={movie.id} className="movie">
+              <Link to={`/movie/${encodeURIComponent(movie.title)}`} className="movie-link">
+
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <div className="movie-info">
+                  <div className="movie-title">{movie.title}</div>
+                  <div className="movie-average">⭐{movie.vote_average}</div>
+                </div>
+                <div className="movie-details">
+                  <h3>{movie.title}</h3>
+                  <p className="overview">{movie.overview}</p>
+                </div>
+              </Link>
+
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default NowPlayingPage;
